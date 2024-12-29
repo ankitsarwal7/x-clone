27dac33ff67f59a12a583318284eb840d111cb4e
ankit kumar <ankitsarwal7@gmail.com>
2024-12-29 18:10:29 +0530

    leftbar section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,18 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import LeftBar from "./components/LeftBar";
+import RightBar from "./components/RightBar"; 
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "x-clone",
@@ -25,15 +27,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-          <div className="flex justify-between">
-
-          <div className="">LeftBar</div>
-          <div className="">{children}</div>
-          <div className="">RightBar</div>
-
+         <div className="max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto flex justify-between">
+          <div className="px-2 xsm:px-4 xxl:px-8 ">
+            <LeftBar />
           </div>
+          <div className="flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray ">
+            {children}
+            {/* {modal} */}
+          </div>
+          <div className="hidden lg:flex ml-4 md:ml-8 flex-1 ">
+            <RightBar />
+          </div>
+        </div>
       </body>
     </html>
   );
